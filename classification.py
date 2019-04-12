0be9d80a2c80b78ff5c1f7392e9b6c1e11ef81b1
ssz66666 <ssz66666@gmail.com>
2019-04-12 19:58:05 +0100

    turn on parallel training

--- classification.py
+++ classification.py
@@ -7,5 +7,5 @@ from dataset_util import uci_mhealth
 with sqlite3.connect('test.db') as conn:
    mhealth = pd.read_sql_query(uci_mhealth.raw_table_query, conn)
 x, y = uci_mhealth.to_classification(mhealth)
-clsf = RandomForestClassifier(n_estimators=500, class_weight="balanced")
+clsf = RandomForestClassifier(n_estimators=500, class_weight="balanced", n_jobs=-1)
 clsf.fit(x,y)
