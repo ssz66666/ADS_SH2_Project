96e9dac1570d976e3bc52ae21aded2fc56fc0c45
ssz66666 <ssz66666@gmail.com>
2019-04-12 16:20:10 +0100

    remove writing to csv in test loader

--- dataset_loader/test/loader.py
+++ dataset_loader/test/loader.py
@@ -9,20 +9,20 @@ with sqlite3.connect('test.db') as conn:
     # data = mhealth.load_dataset_to_mem(path)
     mhealth.load_dataset_to_sqlite(conn.cursor(), path)
 
-    sql_samples = conn.execute("SELECT * FROM {}".format(mhealth.samples_table))
-    sql_sensor = conn.execute("SELECT * FROM {}".format(mhealth.sensor_readings_table))
+    # sql_samples = conn.execute("SELECT * FROM {}".format(mhealth.samples_table))
+    # sql_sensor = conn.execute("SELECT * FROM {}".format(mhealth.sensor_readings_table))
 
     conn.commit()
 
-    with open('samples.csv', 'w') as f1:
-        writer = csv.writer(f1)
-        writer.writerow(mhealth.samples_schema.keys())
-        writer.writerows(sql_samples)
+    # with open('samples.csv', 'w') as f1:
+    #     writer = csv.writer(f1)
+    #     writer.writerow(mhealth.samples_schema.keys())
+    #     writer.writerows(sql_samples)
 
 
-    with open('sensors.csv', 'w') as f2:
-        writer2 = csv.writer(f2)
-        writer2.writerow(list(mhealth.sensor_readings_schema.keys())[:-2])
-        writer2.writerows(sql_sensor)
+    # with open('sensors.csv', 'w') as f2:
+    #     writer2 = csv.writer(f2)
+    #     writer2.writerow(list(mhealth.sensor_readings_schema.keys())[:-2])
+    #     writer2.writerows(sql_sensor)
 
 
