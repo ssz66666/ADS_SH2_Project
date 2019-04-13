8e50e516ec59a7bd13476f9e761189603847b961
ssz66666 <ssz66666@gmail.com>
2019-04-13 01:13:15 +0100

    update classification.py with config

--- classification.py
+++ classification.py
@@ -5,9 +5,10 @@ import sqlite3
 from dataset_util import uci_mhealth
 import pickle
 
+from config import SQLITE_DATABASE_FILE
 
 def main():
-    with sqlite3.connect('test.db') as conn:
+    with sqlite3.connect(SQLITE_DATABASE_FILE) as conn:
         mhealth = pd.read_sql_query(uci_mhealth.raw_table_query, conn)
     x, y = uci_mhealth.to_classification(mhealth)
     clsf = RandomForestClassifier(n_estimators=500, class_weight="balanced", n_jobs=-1)
