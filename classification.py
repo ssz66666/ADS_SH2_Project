a44381e17923117f7caff10f31bad7b86c6fc66d
HAN-Siyu <hansy15@mails.jlu.edu.cn>
2019-04-13 16:26:03 +0100

    Update classification.py
    Just add train_test_split

--- classification.py
+++ classification.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
+import sklearn.model_selection
 import pandas as pd
 import sqlite3
 
@@ -13,8 +14,11 @@ def main():
     x, y = uci_mhealth.to_classification(mhealth)
     clsf = RandomForestClassifier(n_estimators=500, class_weight="balanced", n_jobs=-1)
     clsf.fit(x,y)
+    RF_pred = clsf.predict(X_test)
     with open('training_result.pickle', 'wb') as f:
         pickle.dump(clsf, f)
 
+testset = sklearn.model_selection.train_test_split(dataset, test_size = 0.2, random_state = 1, stratify = dataset.iloc[:,1])
+        
 if __name__ == "__main__":
     main()
