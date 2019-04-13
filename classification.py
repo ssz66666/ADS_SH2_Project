e1d94d60dea3790065adcf8d792f971b98ce4e1b
ssz66666 <ssz66666@gmail.com>
2019-04-13 16:55:58 +0100

    comment out useful stuff for later use

--- classification.py
+++ classification.py
@@ -11,14 +11,14 @@ from config import SQLITE_DATABASE_FILE
 def main():
     with sqlite3.connect(SQLITE_DATABASE_FILE) as conn:
         mhealth = pd.read_sql_query(uci_mhealth.raw_table_query, conn)
-    x, y = uci_mhealth.to_classification(mhealth)
+    X, y = uci_mhealth.to_classification(mhealth)
     clsf = RandomForestClassifier(n_estimators=500, class_weight="balanced", n_jobs=-1)
-    clsf.fit(x,y)
-    RF_pred = clsf.predict(X_test)
+    clsf.fit(X,y)
+    # RF_pred = clsf.predict(X_test)
     with open('training_result.pickle', 'wb') as f:
         pickle.dump(clsf, f)
 
-testset = sklearn.model_selection.train_test_split(dataset, test_size = 0.2, random_state = 1, stratify = dataset.iloc[:,1])
+# testset = sklearn.model_selection.train_test_split(dataset, test_size = 0.2, random_state = 1, stratify = dataset.iloc[:,1])
         
 if __name__ == "__main__":
     main()
