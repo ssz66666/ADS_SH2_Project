828eb1b1791c0643aab2435e562af24fbd8a0394
ssz66666 <ssz66666@gmail.com>
2019-04-15 18:31:00 +0100

    minor script fix dealing with sys.argv

--- load_dataset.py
+++ load_dataset.py
@@ -25,13 +25,13 @@ def load_all_datasets(conn, path, *datasets):
 
 NEW_PAMAP2_PATH = "raw_dataset/uci_pamap2/PAMAP2_finalDataset"
 
-def main():
+def main(argv):
     with sqlite3.connect(SQLITE_DATABASE_FILE) as conn:
         load_all_datasets(conn, RAW_DATASET_DIR, *DATASET_LIST)
-        if "--update-pamap2" in sys.argv:
+        if "--update-pamap2" in argv:
             print("Force updating PAMAP2 dataset from %s" % NEW_PAMAP2_PATH)
             m = get_dataset_loader_module("uci_pamap2")
             m._HACK_force_store_updated_dataset_to_sql(conn.cursor(), NEW_PAMAP2_PATH)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
