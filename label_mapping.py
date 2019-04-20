0dcbb4929fd967b0d443aac74cc6eb8b060b1a09
Panagiotis Dimitriou <panosucycs@gmail.com>
2019-04-20 20:44:54 +0100

    label mapping functions, not finished yet

--- label_mapping.py
+++ label_mapping.py
@@ -1,17 +1,43 @@
 from cluster_comparison import cv_main
+import pandas as pd
 
-df_mapping = cv_main()
 
-print(df_mapping)
+def label_mapping(df_mapping):#,dataset_features):
 
-mapping = []
-maps = []
-for i in range(0,6):
-    max_index = df_mapping.iloc[:,i].idxmax(axis = 0) +1
-    maps.append(i) # pamap
-    maps.append
+    mapping = []
+    maps = []
+    for i in range(0,6):
+        max_index = df_mapping.iloc[:,i].idxmax(axis = 0) +1 # mhealth
+        maps.append(i+1) # pamap
+        maps.append(max_index)
 
-    print(max_index)
+        mapping.append(maps)
+        maps = []
+
+    maps = [5,10]
+    mapping.append(maps)
+
+    for i in mapping:
+        print(i)
+    return mapping
+
+#def pamap_label_mapping(df_mapping,pamap_dataset):
+
+    #for i in range(7,)
+    #subject_id = pamap_dataset.loc[pamap_dataset['activity_id'] == i]
+
+if __name__ == "__main__":
+    df_mapping = cv_main()
+
+    mapping = label_mapping(df_mapping)
+    #print(df_mapping)
+    #exit(0)
+    pamap_dataset = pd.read_pickle("pamap_features.pkl")
+
+
+
+
+    pd.read_pickle("mhealth_features.pkl")
 
 
 
